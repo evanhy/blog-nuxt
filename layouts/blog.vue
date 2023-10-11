d4eeb8e11fcc18401c650d81aa1c0700f61efb65
huyu <yhu8836@gmail.com>
2023-10-11 17:31:34 +0800

    fix: 手机端顶部适配

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -6,7 +6,7 @@ import {navLinks, siteConfig} from "~/site.config";
 <template>
     <section class="min-h-screen mx-auto max-w-65ch 2xl:max-w-85ch pt-12 pb-8 px-8">
         <header class="flex justify-between items-center mb-20">
-            <a class="text-2xl font-hand op-60" href="/">{{ siteConfig.author }}</a>
+            <a class="text-2xl font-hand op-60 hidden sm:block" href="/">{{ siteConfig.author }}</a>
             <nav class="flex justify-around rounded-full shadow-2xl shadow-gray-400 px-3">
                 <nuxt-link v-for="item in navLinks" :key="item.path" :to="item.path"
                            class="py-3 px-5 transition-transform duration-200 hover:scale-125">
@@ -14,7 +14,7 @@ import {navLinks, siteConfig} from "~/site.config";
                     <span :class="item.icon" class="sm:hidden"></span>
                 </nuxt-link>
             </nav>
-            <dark-mode></dark-mode>
+            <dark-mode class="hidden sm:block"></dark-mode>
         </header>
         <main class="flex-1">
             <slot></slot>
